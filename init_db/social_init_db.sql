312942d53780e74f8416de4fd32a24083ad46ab1
undine_20 <lena_sheina@bk.ru>
2023-12-21 17:40:52 +0300

    update submodules

--- init_db/social_init_db.sql
+++ init_db/social_init_db.sql
@@ -2,8 +2,8 @@ CREATE TYPE RELATION AS ENUM ('SEND_FIRST', 'SEND_SECOND', 'FRIENDS');
 
 CREATE TABLE user_data(
     id UUID PRIMARY KEY UNIQUE NOT NULL,
-    name VARCHAR(300) NOT NULL,
-    birth_date TIMESTAMP NOT NULL,
+    name VARCHAR(300),
+    birth_date TIMESTAMP,
     avatar_url VARCHAR(300),
     cover_url VARCHAR(300),
     bio VARCHAR(500)
