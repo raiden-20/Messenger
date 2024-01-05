fecaed5cf568aa12f5a849f82cf02401e400052a
undine_20 <lena_sheina@bk.ru>
2024-01-05 20:28:48 +0300

    change init df files

--- init_db/postgres_init_db.sql
+++ init_db/postgres_init_db.sql
@@ -1,3 +1,46 @@
+CREATE DATABASE auth_db;
+
+\c auth_db;
+
+CREATE TABLE user_credentials(
+    id UUID PRIMARY KEY UNIQUE NOT NULL,
+    email VARCHAR(300) UNIQUE NOT NULL,
+    nickname VARCHAR(300) UNIQUE NOT NULL,
+    hash_password VARCHAR(300) NOT NULL,
+    account_status BOOLEAN NOT NULL DEFAULT FALSE,
+    password_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE service_data(
+    user_id UUID PRIMARY KEY UNIQUE NOT NULL,
+    key INT NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
+CREATE DATABASE social_db;
+
+\c social_db;
+
+CREATE TABLE user_data(
+    id UUID PRIMARY KEY UNIQUE NOT NULL,
+    name VARCHAR(300),
+    birth_date TIMESTAMP,
+    avatar_url VARCHAR(300),
+    cover_url VARCHAR(300),
+    bio VARCHAR(500)
+);
+
+CREATE TABLE user_relation(
+    id INT PRIMARY KEY UNIQUE GENERATED ALWAYS AS IDENTITY NOT NULL,
+    first_user UUID NOT NULL REFERENCES user_data(id),
+    second_user UUID NOT NULL REFERENCES user_data(id),
+    status VARCHAR(20) NOT NULL
+);
+
+CREATE DATABASE blog_db;
+
+\c blog_db;
+
 CREATE TABLE user_data(
     id UUID PRIMARY KEY UNIQUE NOT NULL
 );
